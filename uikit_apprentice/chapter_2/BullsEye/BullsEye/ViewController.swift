e8a34a06b2f0241d42e60eee74828c99170816d4
batista <fernandohsbatista@gmail.com>
2021-11-08 15:58:34 -0300

    Finished the one-button app

---
+++ uikit_apprentice/chapter_2/BullsEye/BullsEye/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by Fernando Batista on 31/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func showAlert() {
+        let alert = UIAlertController(
+            title: "Hello World",
+            message: "This is my first app",
+            preferredStyle: .alert
+        )
+        
+        let action = UIAlertAction(
+            title: "Awesome",
+            style: .default,
+            handler: nil
+        )
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    
+}
+
+
+
+
