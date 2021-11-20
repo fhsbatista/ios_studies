1a2b6e9686a69a25904d76811624e153f68fdfef
batista <fernandohsbatista@gmail.com>
2021-11-20 19:13:48 -0300

    Makes every round set a new target value

--- uikit_apprentice/BullsEye/BullsEye/ViewController.swift
+++ uikit_apprentice/BullsEye/BullsEye/ViewController.swift
@@ -13,11 +13,18 @@ class ViewController: UIViewController {
     var targetValue = 0
     
     @IBOutlet var slider: UISlider!
+    @IBOutlet var targetLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        currentValue = lroundf(slider.value)
+        startNewRound()
+    }
+    
+    func startNewRound() {
         targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        targetLabel.text = targetValue
+        slider.value = Float(currentValue)
     }
     
 
@@ -36,10 +43,12 @@ class ViewController: UIViewController {
         )
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        startNewRound()
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
         currentValue = lroundf(slider.value)
+        
     }
     
     
